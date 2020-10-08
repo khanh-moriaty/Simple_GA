60271821a0bac09c63b1acd49dc8022881f0e5b2
moriaty <19520624@gm.uit.edu.vn>
2020-10-08 05:48:48 -0700

    Check for all 10 runs. Eval counts for all fitness calls.

--- creator/breeder.py
+++ creator/breeder.py
@@ -10,8 +10,8 @@ class Breeder:
         self._string_size = string_size
 
     def breed(self, parent1: Individual, parent2: Individual) -> Tuple[Individual, Individual]:
-        child1, child2 = self._single_point_crossover(parent1, parent2)
-        # child1, child2 = self._uniform_crossover(parent1, parent2)
+        # child1, child2 = self._single_point_crossover(parent1, parent2)
+        child1, child2 = self._uniform_crossover(parent1, parent2)
         return child1, child2
 
     def _uniform_crossover(self, parent1: Individual, parent2: Individual) -> Tuple[Individual, Individual]:
